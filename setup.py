f92ebb4c04678a1cfd49a64e54d363063bb08cff
marzzzello <15198431+marzzzello@users.noreply.github.com>
2022-02-18 23:12:56 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name='pytr',
-    version='0.1.1',
+    version='0.1.2',
     description='Use TradeRepublic in terminal',
     long_description=readme(),
     long_description_content_type='text/markdown',
