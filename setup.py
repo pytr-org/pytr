0c3be0f703a32911f26b40155c543e1d472b6c81
britiscurious <alexander.passin@outlook.com>
2022-04-21 11:24:11 +0200

    Increased version to 0.1.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name='pytr',
-    version='0.1.4',
+    version='0.1.5',
     description='Use TradeRepublic in terminal',
     long_description=readme(),
     long_description_content_type='text/markdown',
