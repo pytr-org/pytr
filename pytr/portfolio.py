f32d66b852ecc18878759d497cc8a2c6764f9744
Cyril Pawelko <cyril@pawelko.net>
2024-05-06 21:13:50 +0200

    Get products from correct exchange (issue #48)

--- pytr/portfolio.py
+++ pytr/portfolio.py
@@ -38,28 +38,9 @@ class Portfolio:
 
             await self.tr.unsubscribe(subscription_id)
 
-        # Populate netValue for each ISIN
-        positions = self.portfolio['positions']
-        subscriptions = {}
-        for pos in sorted(positions, key=lambda x: x['netSize'], reverse=True):
-            isin = pos['instrumentId']
-            # subscription_id = await self.tr.instrument_details(pos['instrumentId'])
-            subscription_id = await self.tr.ticker(isin, exchange='LSX')
-            subscriptions[subscription_id] = pos
-
-        while len(subscriptions) > 0:
-            subscription_id, subscription, response = await self.tr.recv()
-
-            if subscription['type'] == 'ticker':
-                await self.tr.unsubscribe(subscription_id)
-                pos = subscriptions[subscription_id]
-                subscriptions.pop(subscription_id, None)
-                pos['netValue'] = float(response['last']['price']) * float(pos['netSize'])
-            else:
-                print(f"unmatched subscription of type '{subscription['type']}':\n{preview(response)}")
-
         # Populate name for each ISIN
         subscriptions = {}
+        positions = self.portfolio['positions']
         for pos in sorted(positions, key=lambda x: x['netSize'], reverse=True):
             isin = pos['instrumentId']
             subscription_id = await self.tr.instrument_details(pos['instrumentId'])
@@ -73,6 +54,26 @@ class Portfolio:
                 pos = subscriptions[subscription_id]
                 subscriptions.pop(subscription_id, None)
                 pos['name'] = response['shortName']
+                pos['exchangeIds'] = response['exchangeIds']
+            else:
+                print(f"unmatched subscription of type '{subscription['type']}':\n{preview(response)}")
+
+        # Populate netValue for each ISIN
+        subscriptions = {}
+        for pos in sorted(positions, key=lambda x: x['netSize'], reverse=True):
+            isin = pos['instrumentId']
+            # subscription_id = await self.tr.instrument_details(pos['instrumentId'])
+            subscription_id = await self.tr.ticker(isin, exchange=pos['exchangeIds'][0])
+            subscriptions[subscription_id] = pos
+
+        while len(subscriptions) > 0:
+            subscription_id, subscription, response = await self.tr.recv()
+
+            if subscription['type'] == 'ticker':
+                await self.tr.unsubscribe(subscription_id)
+                pos = subscriptions[subscription_id]
+                subscriptions.pop(subscription_id, None)
+                pos['netValue'] = float(response['last']['price']) * float(pos['netSize'])
             else:
                 print(f"unmatched subscription of type '{subscription['type']}':\n{preview(response)}")
 
