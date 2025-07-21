83c3db6a616a51f225da93077b2b7ade45212466
Christoph Langer <christoph.langer@sap.com>
2025-07-21 22:03:56 +0200

    fix formatting

--- pytr/portfolio.py
+++ pytr/portfolio.py
@@ -72,8 +72,9 @@ class Portfolio:
                 # We need to calculate the netValue for bonds differently
                 # We need to identify if it is a bond name - bonds have "year like 2025" in their name:
                 bond_pattern = re.compile(
-                    r'(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|April|May|June|July|August|September|October|November|December)\.?\s+20\d{2}',
-                    re.IGNORECASE)
+                    r"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|April|May|June|July|August|September|October|November|December)\.?\s+20\d{2}",
+                    re.IGNORECASE,
+                )
 
                 if bond_pattern.search(pos["name"]):
                     # Bond calculation - price is per $100 face value
