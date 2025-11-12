3618fec2a8fc4833b7f36d92fb5a9aa98306fe48
ExploracuriousAlex <passin.alexander@gmail.com>
2025-11-12 20:13:15 +0100

    remove --no-flat option

--- pytr/main.py
+++ pytr/main.py
@@ -250,7 +250,7 @@ def get_main_parser():
         "--flat",
         default=False,
         help="Do not sort documents into folders and keep their original filenames",
-        action=argparse.BooleanOptionalAction,
+        action="store_true",
     )
 
     # export_transactions
