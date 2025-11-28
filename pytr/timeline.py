7279b529aeffad7438f42b068b89055c15acdfdf
ExploracuriousAlex <passin.alexander@gmail.com>
2025-11-28 18:58:39 +0100

    use subscription id to match timeline details

--- pytr/timeline.py
+++ pytr/timeline.py
@@ -94,7 +94,7 @@ class Timeline:
             elif subscription.get("type", "") == "timelineActivityLog":
                 await self.get_next_timeline_activity_log(response)
             elif subscription.get("type", "") == "timelineDetailV2":
-                await self.process_timelineDetail(response)
+                await self.process_timelineDetail(response, subscription.get("id"))
             else:
                 self.log.warning(f"unmatched subscription of type '{subscription['type']}':\n{preview(response)}")
 
@@ -200,7 +200,8 @@ class Timeline:
 
             self.requested_detail += 1
             if msg is None:
-                await self.tr.timeline_detail_v2(event["id"])
+                subscription_id = await self.tr.timeline_detail_v2(event["id"])
+                event["timeline_detail_subscription_id"] = subscription_id
             else:
                 self.received_detail += 1
                 self.events.append(event)
@@ -228,12 +229,13 @@ class Timeline:
             except StopAsyncIteration:
                 pass
 
-    async def process_timelineDetail(self, response):
+    async def process_timelineDetail(self, response, subscription_id):
         """
         process timeline details response
         """
 
-        event = self.timeline_details.get(response.get("id", "dummy"), None)
+        event = self.timeline_details.get(subscription_id, None)
+
         if event is None:
             self.log.warning(f"Ignoring unrequested event response {json.dumps(response, indent=4)}")
             self.skipped_detail += 1
